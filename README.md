eec5c1c477838ffe6d6343f3d69fd35dcd1ab8fd
sarbanandadev <sarbanandadev@gmail.com>
2022-06-11 13:58:05 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Basic Progressive Web Application
+
+Just a demo of the basic PWA service worker features
+
+<!-- https://www.pwabuilder.com -->
+<!-- https://www.pwabuilder.com/imageGenerator -->
+
+
+<!-- echo "# PWA-App" >> README.md -->
+<!-- git init -->
+<!-- git add README.md -->
+<!-- git commit -m "first commit" -->
+<!-- git branch -M main -->
+<!-- git remote add origin https://github.com/sarbanandadev/PWA-App.git -->
+<!-- git push -u origin main -->
+
+<!-- …or push an existing repository from the command line -->
+<!-- git remote add origin https://github.com/sarbanandadev/PWA-App.git -->
+<!-- git branch -M main -->
+<!-- git push -u origin main -->
---
+++ server.js
@@ -0,0 +1,44 @@
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+
+const port = 3636;
+const server = http.createServer((req, res) => {
+  let parsedURL = url.parse(req.url, true);
+  let path = parsedURL.path.replace(/^\/+|\/+$/g, "");
+
+  if (path == "") {
+    path = "index.html";
+  }
+  console.log(`Requested path ${path} `);
+
+  let file = __dirname + "/" + path;
+  fs.readFile(file, function (err, content) {
+    if (err) {
+      console.log(`File Not Found ${file}`);
+      res.writeHead(404);
+      res.end();
+    } else {
+      console.log(`Returning ${path}`);
+      res.setHeader("X-Content-Type-Options", "nosniff");
+      switch (path) {
+        case "css/app.css":
+          res.writeHead(200, { "Content-type": "text/css" });
+          break;
+        case "js/app.js":
+          res.writeHead(200, { "Content-type": "application/javascript" });
+          break;
+        case "index.html":
+          res.writeHead(200, { "Content-type": "text/html" });
+          break;
+        case "404.html":
+          res.writeHead(200, { "Content-type": "text/html" });
+      }
+      res.end(content);
+    }
+  });
+});
+
+server.listen(port, "localhost", () => {
+  console.log(`Listening on port localhost:${port}`);
+});
